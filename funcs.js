29bc17f491274bb40841eaad2a4b952125015503
haykam821 <24855774+haykam821@users.noreply.github.com>
2019-08-11 13:51:50 -0400

    Fix ESLint errors

--- funcs.js
+++ funcs.js
@@ -3,15 +3,15 @@ const funcs = {};
 funcs.randInt = (min, max) => ~~(Math.random() * (max - min)) + min;
 funcs.randItem = choices => choices[funcs.randInt(0, choices.length)];
 funcs.makeEvalImage = script => {
-    return `<img src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7" onload="${script}">`
+	return `<img src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7" onload="${script}">`;
 };
 funcs.flatten = function(arr) {
-    return arr.reduce((flat, toFlatten) =>
-        flat.concat(Array.isArray(toFlatten) ? flatten(toFlatten) : toFlatten), []);
-}
+	return arr.reduce((flat, toFlatten) =>
+		flat.concat(Array.isArray(toFlatten) ? funcs.flatten(toFlatten) : toFlatten), []);
+};
 funcs.idGenerator = function*() {
-    var _id = 0;
-    while (true) yield _id++;
-}
+	let _id = 0;
+	while (true) yield _id++;
+};
 
 module.exports = funcs;
